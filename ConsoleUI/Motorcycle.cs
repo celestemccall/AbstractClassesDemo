6b70ba9c8dba3f1a276b1795b1092958805f3f0d
Celeste McCall <celestemccall93@gmail.com>
2023-02-03 19:37:02 -0600

    Initial Commit

---
+++ ConsoleUI/Motorcycle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ConsoleUI
+{
+	public class Motorcycle : Vehicle
+	{
+        public bool HasSideCart { get; set; } = false;
+
+        public override void DriveAbstract()
+        {
+            Console.WriteLine("This motorcyle is in drive");
+        }
+
+        public override void DriveVirtual()
+        {
+            Console.WriteLine("This motorcyle has been fixed and is now driveable");
+        }
+    }
+}
+
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -4,47 +4,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ConsoleUI
 {
     class Program
     {
         static void Main(string[] args)
         {
-            /*
-             * Todo follow all comments!! 
-             */
+
 
             #region Vehicles
 
-            /*
-             * Create an abstract class called Vehicle
-             * The vehicle class shall have three string properties Year, Make, and Model
-             * Set the defaults to something generic in the Vehicle class
-             * Vehicle shall have an abstract method called DriveAbstract with no implementation
-             * Vehicle shall have a virtual method called DriveVirtual with a base implementation.
-             */
 
-            /* 
-             * Now create 2 non-abstract classes: Car and Motorcycle, that inherit from Vehicle
-             * Add a distict property in the 2 derived classes such as HasTrunk for Car and HasSideCart for Motorcycle
-             * Provide the implementations for the abstract methods
-             * Only in the Motorcycle class will you override the virtual drive method
-            */
 
-            // Create a list of Vehicle called vehicles
+            var vehicles = new List<Vehicle>();
 
-            /*
-             * Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
-             * - set them up as one of each derived class
-             * Set the properties with object initializer syntax
-             */
+            Car infiniti = new Car()
+            {
+                HasTrunk = true,
+                Make = "Infiniti",
+                Model = "G37",
+                Year = 2009
+            };
 
-            /*
-             * Add the 4 vehicles to the list
-             * Using a foreach loop iterate over each of the properties
-             */
+            Motorcycle Ryker = new Motorcycle()
+            {
+                HasSideCart = false,
+                Make = "Can-am",
+                Model = "Ryker 900",
+                Year = 2020
+            };
 
-            // Call each of the drive methods for one car and one motorcycle
+            Vehicle sedan = new Car()
+            {
+                HasTrunk = true,
+                Make = "Ford",
+                Model = "Focus",
+                Year = 2007
+            };
+
+            Vehicle coupe = new Car()
+            {
+                HasTrunk = true,
+                Make = "Acura",
+                Model = "Integra",
+                Year = 1995
+            };
+
+            vehicles.Add(infiniti);
+            vehicles.Add(Ryker);
+            vehicles.Add(sedan);
+            vehicles.Add(coupe);
+
+            foreach(var veh in vehicles)
+            {
+                Console.WriteLine($"Make: {veh.Make} Model: {veh.Model} Year: {veh.Year}");
+                veh.DriveAbstract();
+                veh.DriveVirtual();
+                Console.WriteLine("--------------------------------------");
+            }
+
+
+
 
             #endregion            
             Console.ReadLine();
